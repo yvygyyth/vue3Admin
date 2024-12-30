ca9bfa524ded16fbf98039d98016afc55cb50bc2
虞子楠 <12227713+yu-zinan@user.noreply.gitee.com>
2024-12-30 18:00:56 +0800

    feat(cache): 表单项

--- src/hooks/useCacheData/cacheJson.ts
+++ src/hooks/useCacheData/cacheJson.ts
@@ -1,11 +1,12 @@
 import { agentList, matterList, payChattList, costTypeList } from '@/api/free/index'
+import { type SelectOptionData } from '@arco-design/web-vue'
 import type { CacheDataOptions } from './type'
 import { CDK } from './type'
 import type { ListRes as OtherCostLR } from '@/api/costType/type'
 export const cacheJson: Record<string | number, CacheDataOptions<any, any>> = {
   [CDK.costTypeList]: {
     fetchFunction: costTypeList,
-    transform: (res: OtherCostLR) => {
+    transform: (res: OtherCostLR): SelectOptionData => {
       const { data } = res
       return data.map((item) => ({
         value: item.id,
--- src/views/list/search-table/useTableSearchForm/index.tsx
+++ src/views/list/search-table/useTableSearchForm/index.tsx
@@ -6,25 +6,12 @@ import { useI18n } from 'vue-i18n'
 import { useTableStore } from '../tableStore'
 import { storeToRefs } from 'pinia'
 import { createRangeComputed } from '@/utils/ref'
+import { cacheData } from '@/hooks/useCacheData'
+import { CDK } from '@/hooks/useCacheData/type'
 export const useTableSearchForm = () => {
   const { t } = useI18n()
   const { currentLocale } = useLocale()
   const { searchQuery } = storeToRefs(useTableStore())
-
-  const contentTypeOptions = computed<SelectOptionData[]>(() => [
-    {
-      label: t('searchTable.form.contentType.img'),
-      value: 'img'
-    },
-    {
-      label: t('searchTable.form.contentType.horizontalVideo'),
-      value: 'horizontalVideo'
-    },
-    {
-      label: t('searchTable.form.contentType.verticalVideo'),
-      value: 'verticalVideo'
-    }
-  ])
 
   const colSpan = computed(() => {
     if (currentLocale.value === LocaleOptions.en) return 12
@@ -33,8 +20,8 @@ export const useTableSearchForm = () => {
 
   const formAttrs = {
     labelAlign: 'left',
-    labelCol: { span: 6 },
-    wrapperCol: { span: 18 }
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 }
   }
 
   const searchForm = (
@@ -44,7 +31,23 @@ export const useTableSearchForm = () => {
           <Input v-model={searchQuery.value.hirer_keyword} placeholder={'请填写关键词'} />
         </Form.Item>
       </Grid.Col>
-      <Grid.Col span={12} offset={0}>
+      <Grid.Col span={8}>
+        <Form.Item field="bill_type_id" label={'收支类型'}>
+          <Select
+            placeholder={'请选择收支类型'}
+            v-model={searchQuery.value.bill_type_id}
+            allow-search
+            allow-clear
+          >
+            {cacheData(CDK.costTypeList).value.map((option: SelectOptionData) => (
+              <Select.Option key={option.label} value={option.value}>
+                {option.label}
+              </Select.Option>
+            ))}
+          </Select>
+        </Form.Item>
+      </Grid.Col>
+      <Grid.Col span={8} offset={0}>
         <Form.Item field="start_time" label={'时间范围'}>
           <a-range-picker
             showTime
