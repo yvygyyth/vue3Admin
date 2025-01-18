6a96f6ad33d8a115181738408913682f71af1565
虞子楠 <12227713+yu-zinan@user.noreply.gitee.com>
2025-01-18 15:08:37 +0800

    fix(发布订阅管理器): 类型安全声明

--- src/hooks/useEventBus.ts
+++ src/hooks/useEventBus.ts
@@ -1,17 +1,23 @@
 type Key = string | number | symbol
 
+// 事件类型，事件类型为对象结构
 type EventType = Record<Key, unknown>
 
+// 事件处理器
 export type Handler<T = unknown> = (event: T) => void
 
+// 事件处理器列表
 export type EventHandlerList<T = unknown> = Array<Handler<T>>
 
+// 事件处理器映射的Map结构
+// Map[事件名, 事件处理器列表]
 export type EventHandlerMap<Events extends Record<Key, unknown>> = Map<
   keyof Events,
   EventHandlerList<Events[keyof Events]>
 >
 
-type GenericEventHandler<Events> = Handler<Events[keyof Events]>
+// 事件处理器，但是限制了参数类型
+type EventHandler<Events> = Handler<Events[keyof Events]>
 
 class EventBus<Events extends EventType> {
   all: EventHandlerMap<Events>
@@ -21,7 +27,7 @@ class EventBus<Events extends EventType> {
   }
 
   // 注册事件处理器
-  on<K extends keyof Events>(eventName: K, handler: GenericEventHandler<Events>) {
+  on<K extends keyof Events>(eventName: K, handler: EventHandler<Events>) {
     const handlers = this.all.get(eventName)
     if (handlers) {
       handlers.push(handler)
@@ -31,17 +37,17 @@ class EventBus<Events extends EventType> {
   }
 
   // 触发事件
-  emit<K extends keyof Events>(eventName: K, args: Events[K]) {
+  emit<K extends keyof Events>(eventName: K, args: Events[keyof Events]) {
     const handlers = this.all.get(eventName)
     if (handlers) {
-      ;(handlers as EventHandlerList<Events[K]>)
+      handlers
         .slice() // 复制数组，防止修改原数组
         .map((handler) => handler(args))
     }
   }
 
   // 注销事件处理器
-  off<K extends keyof Events>(eventName: K, handler?: GenericEventHandler<Events>) {
+  off<K extends keyof Events>(eventName: K, handler?: EventHandler<Events>) {
     const handlers = this.all.get(eventName)
     if (handlers) {
       if (handler) {
@@ -53,9 +59,8 @@ class EventBus<Events extends EventType> {
   }
 
   // 注册只能触发一次的事件处理器
-  once<K extends keyof Events>(eventName: K, handler: GenericEventHandler<Events>) {
-    // wrappedCallback 显式指定为 GenericEventHandler<Events>
-    const wrappedCallback = (args: Events[K]) => {
+  once<K extends keyof Events>(eventName: K, handler: EventHandler<Events>) {
+    const wrappedCallback = (args: Events[keyof Events]) => {
       handler(args)
       this.off(eventName, wrappedCallback) // 执行后移除事件处理器
     }
